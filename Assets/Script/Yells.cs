0a7d0e7716b16d2a3792e647ce685a5007dc2f05
4g6wb5mndy <4g6wb5mndy@privaterelay.appleid.com>
2026-05-20 11:30:12 +0900

    ScriptableObjectができました

---
+++ Assets/Script/Yells.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Yells", menuName = "Scriptable Objects/Yells")]
+public class Yells : ScriptableObject
+{
+    public string[] JapaneseYell;
+    public string[] EnglishYell;
+}
