eed6889121027c2d9f14ca1faf49a46d14227f67
junyoko1 <junyoko1@outlook.jp>
2026-05-21 10:43:05 +0900

    BGM SEのenumを追加

--- Assets/Script/Audio/SoundType.cs
+++ Assets/Script/Audio/SoundType.cs
@@ -1,7 +1,23 @@
 ﻿public enum SoundType
 {
     //SEをここに追加
+    TypingSE,
+    TypingSuccessSE,
+    TypingMissSE,
 
+    ResultClearSE,
+
+    YellSE,
+    UISelectSE,
+    QteWarningSE,
+    CountDownSE,
+    JunpSE,
+    CarCrashSE,　
+    GraveapperSE,　// 墓に帰る
+    ReturnGroundSE, //土に帰る
     //BGMをここに追加
-
+    TitleBGM,
+    InGameBGM,
+    ClearBGM,
+    GameOverBGM
 }
