1393916b903102b3a49104970872027cf453534a
AoiTomiyama <TadoO.o2209@gmail.com>
2026-05-25 12:59:13 +0900

    [update] リファクタリング

--- Assets/Script/QTE/CarMovement.cs
+++ Assets/Script/QTE/CarMovement.cs
@@ -3,17 +3,9 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     public float speed = 2.0f;
-    private Vector3 startPosition;
-    private bool movingLeft = true;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        startPosition = transform.position;
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
+        transform.position += Vector3.left * (speed * Time.deltaTime);
     }
 }
